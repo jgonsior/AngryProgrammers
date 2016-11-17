aabf801cd7e0f6b98e23d4f0675b1d6c3e5d8398
Richard <richard.kwasnicki@gmx.de>
2016-11-17 11:11:51 +0100

    added tostring to ingamestate

--- src/ab/demo/other/InGameState.java
+++ src/ab/demo/other/InGameState.java
@@ -4,4 +4,14 @@ package ab.demo.other;
  * Created by Richard on 10.11.2016.
  */
 public class InGameState {
+
+    public String name;
+
+    public InGameState(String name){
+        this.name = name;
+    }
+
+    public String toString(){
+        return name;
+    }
 }
