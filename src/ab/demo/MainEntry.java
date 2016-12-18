a6aab348253ae092c018abb9dbb32752232fcdbe
Richie94 <richard.kwasnicki@gmx.de>
2016-12-18 18:28:33 +0100

    create objects before states because states references it

--- src/ab/demo/MainEntry.java
+++ src/ab/demo/MainEntry.java
@@ -110,8 +110,8 @@ public class MainEntry {
                 qValuesDAO.createQValuesTable();
                 qValuesDAO.createAllGamesTable();
                 qValuesDAO.createAllMovesTable();
-                qValuesDAO.createAllStatesTable();
                 qValuesDAO.createAllObjectsTable();
+                qValuesDAO.createAllStatesTable();
                 qValuesDAO.createAllStateIdsTable();
             }
 
