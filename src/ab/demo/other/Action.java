f2660185ef6dada7ecd54ec05c6585512b82526c
Julius Gonsior <julius@gaenseri.ch>
2017-01-15 23:59:08 +0100

    Sorted Code

--- src/ab/demo/other/Action.java
+++ src/ab/demo/other/Action.java
@@ -23,20 +23,20 @@ public class Action {
         //@todo calculate targetObject!
     }
 
-    public double getScore() {
-        return score;
-    }
-
-    public void setScore(double score) {
-        this.score = score;
-    }
-
     public Action(ABObject targetObject, ABObject.TrajectoryType trajectoryType) {
         //this.id = actionId;
         this.targetObject = targetObject;
         this.targetPoint = targetObject.getCenter();
         this.trajectoryType = trajectoryType;
         setName(targetObject.myToString() + " " + trajectoryType.name());
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
     }
 
     public String getName() {
@@ -49,20 +49,21 @@ public class Action {
 
     /**
      * @todo: set targetObject?!
-     *
-    private void calculateTargetObject() {
-    //calculate targetPoint
-    /*ArrayList<ABObject> shootableObjects = GameState.getProblemState().getPossibleActions();
-
-    //@todo should be removed and it needs to be investigated why nextAction returns sometimes wrong actions!
-    // seems to be error in vision module where it found invisible objects on initialisation
-    /*if (shootableObjects.size() - 1 < this.getId()) {
-    this.setId(shootableObjects.size() - 1);
-    }*
-    targetObject = shootableObjects.get(this.getId());*
-
-    targetPoint = targetObject.getCenter();
-    }*/
+     * <p>
+     * private void calculateTargetObject() {
+     * //calculate targetPoint
+     * /*ArrayList<ABObject> shootableObjects = GameState.getProblemState().getPossibleActions();
+     * <p>
+     * //@todo should be removed and it needs to be investigated why nextAction returns sometimes wrong actions!
+     * // seems to be error in vision module where it found invisible objects on initialisation
+     * /*if (shootableObjects.size() - 1 < this.getId()) {
+     * this.setId(shootableObjects.size() - 1);
+     * }*
+     * targetObject = shootableObjects.get(this.getId());*
+     * <p>
+     * targetPoint = targetObject.getCenter();
+     * }
+     */
 
     public boolean isRand() {
         return rand;
